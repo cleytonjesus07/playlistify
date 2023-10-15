43b554a9fc84af7d8582ce664c69b9c45ae9363e
Cleyton Jesus <cleytonjesus007@gmail.com>
2023-10-15 18:54:55 -0300

    remove priority

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -5,7 +5,7 @@ export default function Card({ container_className, title_className, name, avata
     return (
         <Container className={container_className}>
             <div className="relative bg-senary-color w-36 h-28 my-2">
-                <Image priority={true} alt={name} src={avatar} fill={true} sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw" className="object-cover object-center" />
+                <Image alt={name} src={avatar} fill={true} sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw" className="object-cover object-center" />
             </div>
             <div className={title_className}>
                 {name}
