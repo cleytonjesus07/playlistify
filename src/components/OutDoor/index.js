e9d8007318030d344699c550b8b239c53761e5f6
Cleyton Jesus <cleytonjesus007@gmail.com>
2023-10-18 16:20:24 -0300

    change outdoor tailwind css

--- src/components/OutDoor/index.js
+++ src/components/OutDoor/index.js
@@ -32,14 +32,14 @@ export default function OutDoor({ artists, lang }) {
 
     return (
         <Link href={`/artists/${encodeURL(artistOfTheDay.id)}`}>
-            <div style={{ backgroundImage: "url(" + artistOfTheDay.avatar + ")" }} className="relative flex justify-center items-center w-full h-60 rounded-xl border-8 border-solid border-senary-color bg-senary-color max-md:flex bg-top bg-cover bg-no-repeat max-sm:bg-[100%]">
+            <div style={{ backgroundImage: "url(" + artistOfTheDay.avatar + ")" }} className="relative flex justify-center items-center w-full h-60 rounded-xl border-8 border-solid border-senary-color bg-senary-color max-md:flex bg-top bg-cover bg-no-repeat max-md:bg-[100%]">
                 <div className="z-20 flex h-full w-full">
-                    <div className="relative w-60 h-full max-sm:hidden">
+                    <div className="relative w-60 h-full max-md:hidden">
                         <Image alt={artistOfTheDay.name} src={artistOfTheDay.avatar} fill={true} className="object-cover" />
                     </div>
-                    <div className="flex-1 flex flex-col justify-center items-center max-sm:text-center ">
-                        <span className="text-white font-light mr-10 max-sm:mr-0">{t.Homepage.outdoor_message}</span>
-                        <h3 className="text-white font-bold text-4xl max-sm:text-2xl">{artistOfTheDay.name}</h3>
+                    <div className="flex-1 flex flex-col justify-center items-center max-md:text-center ">
+                        <span className="text-white font-light mr-10 max-md:mr-0">{t.Homepage.outdoor_message}</span>
+                        <h3 className="text-white font-bold text-4xl max-md:text-2xl">{artistOfTheDay.name}</h3>
                     </div>
                 </div>
                 <div className="absolute opacity-60 bg-black w-full h-full"></div>
