6e92fb9b19256f3296a52ee91f3fcb963eb7b04e
Cleyton Jesus <cleytonjesus007@gmail.com>
2023-10-20 14:01:11 -0300

    change tailwind css

--- src/components/Home/mostListening.js
+++ src/components/Home/mostListening.js
@@ -19,15 +19,16 @@ export default async function MostListening({ mostListening, lang }) {
                     topthree?.map(({ Song, Artist }, i) => {
                         return (
                             <Link key={Artist.id} href={`/${lang}/artists/${encodeURL(Artist.id)}-${encodeURL(Song.id)}`} >
-                                <Container className={"flex overflow-x-hidden   items-center justify-center relative  hover:scale-100 scale-95 cursor-pointer transition-all ease-in-out"}>
-                                    {(i == 0) && <FaCrown className="w-6 h-6 absolute right-0 -top-5 rotate-12 text-yellow-500" />}
-                                    <div className=" relative bg-senary-color w-20 h-20 rounded-tl-xl rounded-bl-xl">
+                                <Container className={"flex items-center justify-center relative  hover:scale-100 scale-95 cursor-pointer transition-all ease-in-out"}>
+
+                                    <div className=" relative overflow-hidden bg-senary-color w-20 h-20 rounded-tl-xl rounded-bl-xl">
                                         <Image priority={true} alt={Artist.name} src={Artist.avatar} fill={true} sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw" className="object-cover object-center" />
                                     </div>
                                     <div className="w-36 h-16 flex-1 flex flex-col items-center justify-center text-senary-color px-2 text-center">
                                         <span className="font-extrabold line-clamp-2">{Song.title}</span>
                                         <span className="text-sm line-clamp-1">{Artist.name}</span>
                                     </div>
+                                    {(i == 0) && <FaCrown className="w-6 h-6 absolute right-0 -top-5 rotate-12 text-yellow-500" />}
                                     {(i <= 2) && <span className={`text-white rounded-tr-xl self-start relative  ${(i == 0) ? "bg-yellow-600" : (i == 1) ? "bg-gray-500" : "bg-orange-900"} px-2 font-thin rounded-bl-xl`}>{`${i + 1}°`}</span>}
                                 </Container>
                             </Link>
