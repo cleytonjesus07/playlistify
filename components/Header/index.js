047e6d70f2bb879347681c00477c0572bcce60d3
Cleyton Jesus <cleytonjesus007@gmail.com>
2024-07-22 13:37:35 -0300

    fixing acessibility

--- components/Header/index.js
+++ components/Header/index.js
@@ -18,10 +18,10 @@ export default function Header() {
     return (
         <header className="h-16 flex justify-end items-center bg-[#111] px-2">
             <div className="bg-gradient-to-tr from-[#1e1e1e] to-[#252525] w-32 rounded-md justify-evenly inline-flex items-center [&>button>span]:w-14 max-md:py-2">
-                <button className={`opacity-30 ${pathname.includes("en-us") && '!opacity-100'}`} onClick={() => switcherLanguage("en-us")}>
+                <button title="EUA's flag" className={`opacity-30 ${pathname.includes("en-us") && '!opacity-100'}`} onClick={() => switcherLanguage("en-us")}>
                     <span className="fi fi-us w-7 h-7"></span>
                 </button>
-                <button className={`opacity-30 ${pathname.includes("pt-br") && '!opacity-100'}`} onClick={() => switcherLanguage("pt-br")}><span className="fi fi-br w-7 h-7"></span></button>
+                <button title="Brazil's flag" className={`opacity-30 ${pathname.includes("pt-br") && '!opacity-100'}`} onClick={() => switcherLanguage("pt-br")}><span className="fi fi-br w-7 h-7"></span></button>
             </div>
         </header>
     )
